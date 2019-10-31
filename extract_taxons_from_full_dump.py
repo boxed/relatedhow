df448b0bcc3e1d4a51bea575f42b1cec15505341
Anders Hovmöller <boxed@killingar.net>
2019-10-31 07:53:19 +0100

    Disable ssl verification since it suddenly stopped working
    Former-commit-id: 647cca68cd27d37a2da33274c372254a45d7985e

--- extract_taxons_from_full_dump.py
+++ extract_taxons_from_full_dump.py
@@ -20,9 +20,14 @@
 
 from urllib.request import urlopen
 import gzip
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 with open('taxon_data.json', 'wb') as out:
-    with urlopen('https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.gz') as f:
+    with urlopen('https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.gz', context=ctx) as f:
         with gzip.open(f) as f2:
             for line in f2:
                 if b'"P171"' in line or b'"Q16521"' in line:
