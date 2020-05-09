46145df7c3baed290db359b1cf01cbfaf7a407a1
Anders Hovmöller <boxed@killingar.net>
2020-05-09 13:02:29 +0200

    .

--- relatedhow/settings.py
+++ relatedhow/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'relatedhow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'relatedhow.db',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'relatedhow',
+        'USER': os.environ['DOKKU_MYSQL_MYSQL_ENV_MYSQL_USER'],
+        'PASSWORD': os.environ['DOKKU_MYSQL_MYSQL_ENV_MYSQL_PASSWORD'],
+        'HOST': os.environ['DOKKU_MYSQL_MYSQL_PORT_3306_TCP_ADDR'],
     }
 }
 
